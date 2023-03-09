2fa86ecfac70ec3faba169c09be2dfa231f409f1
= <=>
2023-03-10 00:56:28 +0200

    add Program.cs

---
+++ AQACourse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace  assert_sim
+{
+   static class Program
+    {
+        public static void Main() {
+            int val = 1;
+            Debug.Assert(val != 1, "Value should not be 2");
+            string name = "Anastasiia";
+        }
+    }
+}
